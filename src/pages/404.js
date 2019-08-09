478178ec4654bba04f885ba360b5b7105632eca2
dorshinar <dors718@gmail.com>
2019-08-09 09:44:49 +0300

    simplify 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { graphql } from "gatsby";
+import styled from "styled-components";
 
 import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
 
-class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </>
-    );
-  }
-}
+const Content = styled.div`
+  padding: ${rhythm(0.5)};
+`;
 
-export default NotFoundPage;
+export default () => (
+  <>
+    <SEO title="404: Not Found" />
+    <Content>
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Content>
+  </>
+);
 
 export const pageQuery = graphql`
   query {
