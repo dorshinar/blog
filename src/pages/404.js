c84bce1e2a850f6d855276f029ea1b55e41c70cf
dorshinar <dors718@gmail.com>
2019-08-26 20:48:42 +0300

    add space

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ const Content = styled.div`
 
 export default () => (
   <>
-    <SEO title="404: Not Found" />
+    <SEO title="404:  Not Found" />
     <Content>
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
