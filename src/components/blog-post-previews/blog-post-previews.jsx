f6059f14f6fce939260792ff2673662ad7213845
Dor Shinar <dors718@gmail.com>
2019-11-30 19:26:38 +0200

    junk in layout

--- src/components/blog-post-previews/blog-post-previews.jsx
+++ src/components/blog-post-previews/blog-post-previews.jsx
@@ -3,13 +3,19 @@ import React from "react";
 import { BlogPostPreview } from "../blog-post-preview";
 import { Separator } from "../../utils/styled/separator";
 
-export default ({ posts }) =>
-  posts.map(({ node }, index, array) => {
-    const title = node.frontmatter.title || node.fields.slug;
-    return (
-      <React.Fragment key={node.fields.slug}>
-        <BlogPostPreview node={node} title={title} />
-        {index !== array.length - 1 && <Separator />}
-      </React.Fragment>
-    );
-  });
+export default ({ posts }) => {
+  return (
+    <>
+      <div>Hi you all!</div>
+      {posts.map(({ node }, index, array) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <React.Fragment key={node.fields.slug}>
+            <BlogPostPreview node={node} title={title} />
+            {index !== array.length - 1 && <Separator />}
+          </React.Fragment>
+        );
+      })}
+    </>
+  );
+};
