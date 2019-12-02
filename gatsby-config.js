f1b5fdafd598acb23205522981100740fc680b43
Dor Shinar <dors718@gmail.com>
2019-12-02 20:00:42 +0200

    a

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Dor Shinar`,
     author: `Dor Shinar`,
+    a: "",
     description: `My Personal blog for thoughts.`,
     siteUrl: "https://dorshinar.me",
     social: {
