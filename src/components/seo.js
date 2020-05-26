d1d3eee0cdeaec77fc042113c9910c6622d5c945
Dor Shinar <dors718@gmail.com>
2020-05-26 22:18:19 +0300

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,7 +60,7 @@ function SEO({ description, lang, meta, title, slug, thumbnail }) {
 
       {/* Twitter Card Tags */}
       <meta name="twitter:card" content={"summary"} />
-      <meta name="twitter:creator" content={site.siteMetadata.author} />
+      <meta name="twitter:creator" content={`@{site.siteMetadata.author}`} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       {thumbnail && (
