66b7f2a3446f4f46ab70383d30fc41c4886d6944
Dor Shinar <dors718@gmail.com>
2020-05-31 12:08:11 +0300

    convert to function

--- src/components/scroll-indicator/scroll-indicator.jsx
+++ src/components/scroll-indicator/scroll-indicator.jsx
@@ -1,35 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useCallback } from "react";
 
 import { Container, Indicator } from "./scroll-indicator.styled";
 
-export default class ScrollIndicator extends React.Component {
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+export function ScrollIndicator() {
+  const [width, setWidth] = useState(0);
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
-  state = {
-    width: 0,
-  };
-
-  handleScroll = () => {
+  const handleScroll = useCallback(() => {
     const winScroll =
       document.body.scrollTop || document.documentElement.scrollTop;
     const height =
       document.documentElement.scrollHeight -
       document.documentElement.clientHeight;
     const scrolled = (winScroll / height) * 100;
-    this.setState({ width: scrolled });
-  };
+    setWidth(scrolled);
+  }, []);
 
-  render() {
-    return (
-      <Container>
-        <Indicator width={this.state.width} />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Indicator width={width} />
+    </Container>
+  );
 }
