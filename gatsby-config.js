456daa32df95ca873e9b7d164196e5b3d8c5b1ea
Dor Shinar <dors718@gmail.com>
2020-10-20 17:54:09 +0300

    always use "https://dorshinar.me" as siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,7 @@ module.exports = {
     title: `Dor Shinar`,
     author: `Dor Shinar`,
     description: `My Personal blog for thoughts.`,
-    siteUrl:
-      (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
-      "https://dorshinar.me",
+    siteUrl: "https://dorshinar.me",
     social: {
       github: `dorshinar`,
       linkedin: "dor-shinar-82b00b144",
