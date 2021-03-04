deca9daa2094273ea5ef4a51186786762fab314a
Dor Shinar <dors718@gmail.com>
2021-03-04 11:14:08 +0200

    remove log from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,7 +35,6 @@ exports.createPages = ({ graphql, actions }) => {
     const posts = result.data.allMarkdownRemark.edges;
 
     posts.forEach((post, index) => {
-      console.log({ post, index, slug: post.node.fields.slug });
       const previous =
         index === posts.length - 1 ? null : posts[index + 1].node;
       const next = index === 0 ? null : posts[index - 1].node;
