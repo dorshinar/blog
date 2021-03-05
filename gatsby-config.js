46a21b3c3f693d81b2d4b099ca24789a39e23e41
Dor Shinar <dor.shinar@tonkean.com>
2021-03-05 14:55:17 +0200

    fix all

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,8 +101,8 @@ module.exports = {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
                   custom_elements: [{ "content:encoded": edge.node.html }],
                 });
               });
@@ -116,10 +116,10 @@ module.exports = {
                   node {
                     excerpt
                     html
-                    fields { slug }
                     frontmatter {
                       title
                       date
+                      slug
                     }
                   }
                 }
