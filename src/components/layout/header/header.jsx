9e15f69a5c88d942f33d7d829ee01295676db968
Dor Shinar <dor.shinar@tonkean.com>
2021-03-07 10:53:49 +0200

    add drag to header link

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Link } from "gatsby";
+import { motion } from "framer-motion";
 
-import "./header.css";
 import { ThemeButton } from "../theme-button/themeButton";
+import "./header.css";
+
+const MotionLink = motion(Link);
 
 export function Header() {
   return (
     <header>
       <span className="header-content">
-        <Link className="home-link" to="/" data-p="home-link">
+        <MotionLink
+          className="home-link"
+          to="/"
+          data-p="home-link"
+          dragConstraints={{
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+          drag
+        >
           <h1>Dor Shinar</h1>
-        </Link>
+        </MotionLink>
         <ThemeButton />
       </span>
     </header>
