e99ab79a0c6b1b6c1dd67b9303803c5e4a694499
Dor Shinar <dor.shinar@tonkean.com>
2021-03-22 20:52:58 +0200

    add cover image for infinite scroll

--- gatsby-node.js
+++ gatsby-node.js
@@ -104,6 +104,9 @@ exports.createSchemaCustomization = ({ actions }) => {
       date: Date @dateformat
       slug: String
       published: Boolean
+      cover_image_credit: String
+      cover_image: String
+      cover_image_alt: String
     }
     type Fields {
       readingTime: ReadingTime
