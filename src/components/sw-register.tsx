d965252d34c39d818c7773d05a771efc2d2e8c5c
Dor Shinar <dors718@gmail.com>
2023-08-25 12:43:35 +0300

    clear sw

---
+++ src/components/sw-register.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+
+export function ServiceWorkerRegister() {
+  useEffect(() => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) =>
+        console.log(
+          "Service Worker registration successful with scope: ",
+          registration.scope,
+        ),
+      )
+      .catch((err) => console.log("Service Worker registration failed: ", err));
+  }, []);
+
+  return <></>;
+}
