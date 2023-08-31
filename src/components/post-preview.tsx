d5b8c88f884cbb22471f1ea104ff3f42b9ca3fa4
Dor Shinar <dors718@gmail.com>
2023-08-31 20:19:28 +0300

    small fixes

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 interface Props {
   post: PostMeta;
   cover: StaticImageData;
+  priority: boolean;
 }
 
 export function PostPreview({ post, cover }: Props) {
@@ -13,13 +14,13 @@ export function PostPreview({ post, cover }: Props) {
     <Link
       href={`posts/${post.slug}`}
       className={clsx(
-        "group rounded-lg overflow-hidden w-full transition-[transform,shadow]",
-        "hover:ring-2 focus-within:ring-2 focus:ring-2 ring-offset-zinc-900 ring-emerald-500 ring-offset-8",
+        "group rounded-2xl overflow-hidden w-full transition-[transform,shadow]",
+        "hover:ring-2 focus-within:ring-2 focus:ring-2 ring-offset-zinc-900 ring-emerald-500 p-2",
         "hover:-translate-y-2 focus-within:-translate-y-2 focus:-translate-y-2",
       )}
     >
       {cover && (
-        <span className="aspect-[3] block relative mb-2">
+        <span className="aspect-[3] block relative mb-2 rounded-lg overflow-hidden">
           <Image
             src={cover}
             alt="cover image"
