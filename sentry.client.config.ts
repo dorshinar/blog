41810c92261fd8e96f16f13ed709ab826d0f953f
Dor Shinar <dors718@gmail.com>
2024-01-22 19:53:21 +0200

    test sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,9 +2,9 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import { init, Replay } from "@sentry/nextjs";
 
-Sentry.init({
+init({
   dsn: "https://1275545d067f3905e4aa61ce39fa8fa1@o4506615994384384.ingest.sentry.io/4506615996088320",
 
   // Adjust this value in production, or use tracesSampler for greater control
@@ -21,7 +21,7 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    new Sentry.Replay({
+    new Replay({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
       blockAllMedia: true,
